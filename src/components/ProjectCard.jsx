5be179abe69aabaf5ec775827359cac5ba38e3fa
Morten Johansson <morten1.kodehode@gmail.com>
2022-12-08 09:57:00 +0100

    added border and changed text color on overlay links in cards.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,9 +5,9 @@ function ProjectCard({ title, image, tools, github, live}) {
     <div className="border-2 border-orange-400 rounded-md overflow-hidden relative">
       <img src={image} alt="Project Image" className="block w-full h-36 object-cover rounded-t-lg cursor-pointer"/>
       <div  className="overlay absolute inset-0 h-36 w-full opacity-0 hover:opacity-90 bg-purple-900 duration-500 ease-in-out">
-        <div className=" text text-orange-400 text-base h-36 flex justify-evenly items-center">
-          <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href={live} target="_blank" rel="noopener noreferrer">Live</a>
+        <div className=" text text-white text-base h-36 flex justify-evenly items-center">
+          <a href={github} target="_blank" rel="noopener noreferrer" className="border-2 border-orange-400 rounded-md p-2">GitHub</a>
+          <a href={live} target="_blank" rel="noopener noreferrer" className="border-2 border-orange-400 rounded-md p-2">Live</a>
           </div>
       </div>
       <div className="w-full p-4 bg-purple-900">
