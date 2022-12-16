fe5d135686a2ef2550a7fc501f5f7b52195e3ac1
Morten Johansson <morten1.kodehode@gmail.com>
2022-12-16 09:40:59 +0100

    Updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,25 @@ import React from "react";
 function Hero() {
   return (
     <div className="flex justify-center flex-col px-3 md:px-10 lg:px-40 h-screen bg-hero bg-cover bg-center bg-no-repeat bg-black bg-opacity-60 bg-blend-overlay font-lato">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl text-white pb-5">Hi, My Name is Morten.
-          <br /> 
-          <span className="text-orange-400">I am a Fullstack Developer who loves to build stuff.</span>
-        </h1>
-        <p className="text-white">Keep Scrolling to Explore or
-          <a href="#projects" className="border-solid border-2 border-orange-400 rounded-lg p-2 mx-3 text-orange-400">CLICK ME</a> 
-          to jump to the projects.</p>
-      </div>
-  )
+      <h1 className="text-2xl md:text-3xl lg:text-4xl text-white pb-5">
+        Hi, My Name is Morten.
+        <br />
+        <span className="text-orange-400">
+          I am a Front-End Developer who loves to build stuff.
+        </span>
+      </h1>
+      <p className="text-white">
+        Keep Scrolling to Explore or
+        <a
+          href="#projects"
+          className="border-solid border-2 border-orange-400 rounded-lg p-2 mx-3 text-orange-400"
+        >
+          CLICK ME
+        </a>
+        to jump to the projects.
+      </p>
+    </div>
+  );
 }
 
 export default Hero;
