8970e7ae45c2663c447d65d72cc3cacf1551eccf
Morten Johansson <morten1.kodehode@gmail.com>
2023-03-07 10:55:12 +0100

    added a loading page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import Loading from "./components/LoadingOverlay";
 
 function App() {
-
   return (
     <div>
+      <Loading />
       <Hero />
       <About />
       <Projects />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LoadingOverlay.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { css } from "@emotion/react";
+import { MoonLoader } from "react-spinners";
+
+const LoadingOverlay = () => {
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHidden(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `;
+
+  return (
+    <div
+      className={`fixed inset-0 bg-hero bg-cover bg-center bg-no-repeat bg-black bg-opacity-60 bg-blend-overlay z-50 flex justify-center items-center overflow-hidden transition-opacity ${
+        hidden ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <div>
+        <MoonLoader
+          css={override}
+          size={60}
+          color={"#FB923C"}
+          loading={true}
+          speedMultiplier={0.5}
+        />
+        <h1 className="text-orange-400 font-lato text-xl animate-[pulse_1.3s_ease-in-out_infinite] pt-[25px]">
+          Loading...
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingOverlay;
